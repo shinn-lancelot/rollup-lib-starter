c2ba726ca499a4fa7825ead4a59503cf48994b7a
shinn lancelot <945226793@qq.com>
2018-12-17 15:01:13 +0800

    1. main.js代码拆分成main.js,core.js

---
+++ src/core.js
@@ -0,0 +1,20 @@
+import defaultOptions from './defaultoptions'   // 导入默认选项
+import common from './common'   // 导入通用静态函数类
+
+export default class rollupLib {
+    /**
+     * 初始化构造函数
+     * @param {*} options 
+     */
+    constructor(options = {}) {
+        this.options = common.extend(defaultOptions, options);    // 扩展选项
+    }
+
+    hello() {
+        console.log('hello ' + this.options.content)
+    }
+
+    play(arg) {
+        common.play(arg)    // 调用通用静态类的play函数
+    }
+}
--- src/main.js
+++ src/main.js
@@ -1,20 +1,3 @@
-import defaultOptions from './defaultoptions'   // 导入默认选项
-import common from './common'   // 导入通用静态函数类
+import core from './core'
 
-export default class rollupLib {
-    /**
-     * 初始化构造函数
-     * @param {*} options 
-     */
-    constructor(options = {}) {
-        this.options = common.extend(defaultOptions, options);    // 扩展选项
-    }
-
-    hello() {
-        console.log('hello ' + this.options.content)
-    }
-
-    play(arg) {
-        common.play(arg)    // 调用通用静态类的play函数
-    }
-}
+export default core
