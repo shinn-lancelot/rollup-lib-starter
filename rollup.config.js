991edc5e0bd82e1540e7afb89114775d20bb0c0c
shinn lancelot <945226793@qq.com>
2018-12-17 10:35:22 +0800

    1. 基本完成架构

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,7 @@ import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import buble from 'rollup-plugin-buble'
 import flow from 'rollup-plugin-flow-no-whitespace'
-// import { terser } from 'rollup-plugin-terser'
-import { uglify } from 'rollup-plugin-uglify'
-import { minify } from 'uglify-js'
+import { terser } from 'rollup-plugin-terser'
 import pkg from './package.json'
 
 const production = !process.env.ROLLUP_WATCH
@@ -17,34 +15,59 @@ const banner =
     ' */';
 const libName = 'rollupLib' || pkg.name
 
-export default {
-    input: 'src/main.js',
-    output: [
-        {
-            file: pkg.browser,
-            format: 'umd',
-            banner: banner,
-            name: libName
-        },
-        {
-            file: pkg.main,
-            format: 'cjs',
-            banner: banner,
-            name: libName
-        },
-        {
-            file: pkg.module,
-            format: 'es',
-            banner: banner,
-            name: libName
-        }
-    ],
-    plugins: [
-        resolve(),
-        commonjs(),
-        buble(),
-        flow(),
-        // production && terser(),
-        production && uglify({}, minify)
-    ]
-};
+export default [
+    {
+        input: 'src/main.js',
+        output: [
+            {
+                file: pkg.main,
+                format: 'cjs',
+                banner: banner,
+                name: libName
+            }
+        ],
+        plugins: [
+            resolve(),
+            commonjs(),
+            buble(),
+            flow(),
+            production && terser()
+        ]
+    },
+    {
+        input: 'src/main.js',
+        output: [
+            {
+                file: pkg.module,
+                format: 'es',
+                banner: banner,
+                name: libName
+            }
+        ],
+        plugins: [
+            resolve(),
+            commonjs(),
+            buble(),
+            flow(),
+            production && terser()
+        ]
+    },
+    {
+        input: 'src/main.js',
+        output: [
+            {
+                file: pkg.browser,
+                format: 'umd',
+                banner: banner,
+                name: libName
+            }
+        ],
+        plugins: [
+            resolve(),
+            commonjs(),
+            buble(),
+            flow(),
+            production && terser()
+        ]
+    }
+]
